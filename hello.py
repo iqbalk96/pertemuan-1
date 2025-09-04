5efd43ccab0d1c9ca04e2d32e24bbb2feb6f2bb5
iqbalk96 <mochamad.iqbal@roketin.com>
2025-09-04 23:06:03 +0700

    pertama kali commit

---
+++ hello.py
@@ -0,0 +1,26 @@
+# String
+nama = 'Iqbal'
+
+# Int
+umur = 20
+
+# Float
+tinggi = 168.7
+
+# Boolean
+statusMenikah = True
+
+# Aritmatika
+a = 1
+b = 2.5
+c = a + b
+
+statusMenikahString = 'Sudah Kawin' if statusMenikah else 'Belum Kawin'
+
+print(f"Nama: {nama}")
+print(umur)
+print(tinggi)
+print(f"Status: {statusMenikahString}")
+# print(f"Status: {'Sudah Kawin' if statusMenikah else 'Belum Kawin'}")
+
+print(c)
