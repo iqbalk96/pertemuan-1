0fd651c80467bc008b7d5afe78f1677144cbeb16
harisY <haris.yoboseyo@gmail.com>
2025-09-05 19:39:21 +0700

    add input file

---
+++ input.py
@@ -0,0 +1,13 @@
+nama = input("Nama: ")
+umur = int(input("Umur: "))
+tinggi = float(input("Tinggi: "))
+
+print("\n=== Hasil Input ===")
+print(f"Nama: {nama}")
+print(f"Umur: {umur}")
+print(f"Tinggi: {tinggi}")
+
+if umur >= 18:
+    print("Anda sudah dewasa")
+else:
+    print("Anda belum dewasa")
