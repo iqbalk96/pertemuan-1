2c26953cdd3b5724b643a56aadbcbbbe583a5324
iqbalk96 <mochamad.iqbal@roketin.com>
2025-09-11 22:29:33 +0700

    feat: belajar loop dan fungsi

---
+++ looping/for-loop.py
@@ -0,0 +1,32 @@
+# print(1)
+# print(2)
+# print(3)
+# print(4)
+# print(5)
+
+# for loop with range
+for a in range(1, 6):
+    # logic skip no 3
+    print(a)
+
+
+# while loop
+# i = variabel
+i = 1
+while i <= 5:
+    # logic
+    print(i)
+    i += 1
+
+# for loop dengan break dan continue
+for i in range(1, 11):
+    # logic untuk break
+    # if i == 5:
+    #     break
+
+    # logic untuk continue
+    if i == 5:
+        continue
+
+    # kita mau melakukan apa
+    print(i) 
